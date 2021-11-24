005c36039e687aa3fc9455e003311369b079b9b2
Ze Lei <zlei1@mail.sfsu.edu>
2021-11-24 15:56:58 -0800

    testing results page

---
+++ results.js
@@ -0,0 +1,70 @@
+var innerhtml = `<header>
+<h1>
+    Results verification page Lei
+</h1>
+</header>
+<div>
+<label for="last-name">last name</label>
+<span id="result-last"></span>
+<script>
+    document.getElementById("result-last").innerHTML = localStorage.lastName;
+</script>
+<label for="first-name">first name</label>
+<span id="result-first"></span>
+<script>
+    document.getElementById("result-first").innerHTML = localStorage.firstName;
+</script>
+</div>
+<div class="dropdown">
+<label for="title">preferred title</label>
+</div>
+<div>
+<label for="">height</label>
+'
+"
+</div>
+<div>
+<label for="phone">phone</label>
+</div>
+<div>
+<label class="form-label" for="Address">Address</label>
+</div>
+<div class="row">
+<div class="col">
+    <div class="form-outline">
+        <label class="form-label" for="city">City</label>
+    </div>
+</div>
+<div class="col">
+    <div class="form-outline">
+        <label class="form-label" for="zip">Zip</label>
+    </div>
+</div>
+</div>
+<div>
+<label for="services">services required</label>
+<br>
+e-mail<input type="checkbox">
+phone<input type="checkbox">
+Facebook<input type="checkbox">
+Tweeter<input type="checkbox">
+surface mail<input type="checkbox">
+personal visit<input type="checkbox">
+</div>
+<div class="dropdown">
+<label for="budget">monthly budget</label>
+
+</div>
+<div class="form-outline">
+<label class="form-label" for="typeEmail">Email</label>
+</div>`;
+
+function validateForm() {
+    sessionStorage.lastName = document.getElementById("last");
+    sessionStorage.firstName = document.getElementById("first");
+    sessionStorage.title = document.getElementById("title");
+    sessionStorage.feet = document.getElementById("feet");
+    sessionStorage.inches = document.getElementById("inches");
+
+    document.getElementById("append").innerHTML = innerhtml;
+}
